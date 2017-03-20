dccdfe41c6099abb21f9c0fd65729c3e72bd2971
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-03-20 09:48:30 -0400

    Fix chimera.rb per brew audit warning

--- Formula/chimera.rb
+++ Formula/chimera.rb
@@ -2,8 +2,8 @@ class Chimera < Formula
   desc "Utility to generate Boost.Python bindings for C++ code"
   homepage "https://github.com/personalrobotics/chimera"
   url "https://github.com/personalrobotics/chimera/archive/4199c955c2145e83ccb622f07562c68d231091be.tar.gz"
-  sha256 "9cb6358b5dd50a3c9fe0e1b7f4e468260c12844b74f1878a95e0e1e9c54200c0"
   version "0.0.1-prerelease"
+  sha256 "9cb6358b5dd50a3c9fe0e1b7f4e468260c12844b74f1878a95e0e1e9c54200c0"
 
   depends_on "cmake" => :build
 
