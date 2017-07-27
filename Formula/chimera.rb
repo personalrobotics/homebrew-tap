a6f0a0af05bcd40b89efa416b6bf2027835ccc35
Jeongseok Lee <jslee02@gmail.com>
2017-07-27 12:49:18 -0700

    Bump version

--- Formula/chimera.rb
+++ Formula/chimera.rb
@@ -2,7 +2,7 @@ class Chimera < Formula
   desc "Utility to generate Boost.Python bindings for C++ code"
   homepage "https://github.com/personalrobotics/chimera"
   url "https://github.com/personalrobotics/chimera/archive/d1c74c2e17a102950042511f035360a1a4775322.tar.gz"
-  version "0.0.1-prerelease2"
+  version "0.0.1-prerelease3"
   sha256 "cbb36eadefd77c4560b5cebf496ac0d48c1c0ca57b22faaee4dda658e70221c0"
   head "https://github.com/personalrobotics/chimera", :branch => "master"
 
