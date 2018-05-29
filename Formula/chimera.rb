345cdea1cccb78f133f9285db5b38911e339c264
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-05-29 10:59:55 -0700

    chimera 0.0.4

--- Formula/chimera.rb
+++ Formula/chimera.rb
@@ -1,8 +1,8 @@
 class Chimera < Formula
   desc "Utility to generate Boost.Python bindings for C++ code"
   homepage "https://github.com/personalrobotics/chimera"
-  url "https://github.com/personalrobotics/chimera/archive/v0.0.3.tar.gz"
-  sha256 "69fd5c514a2e58e0eceed88bd36096bd542b538658793b1af755ac89365c7b0c"
+  url "https://github.com/personalrobotics/chimera/archive/v0.0.4.tar.gz"
+  sha256 "27c6fd2d7d2d0165ffc45bde387bd64fe28ea7b6d900023d72936645b5635ecd"
 
   depends_on "cmake" => :build
   depends_on "boost-python3" => :build # for building tests
