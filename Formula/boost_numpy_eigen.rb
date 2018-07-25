b322de1ddf73dea229431685fcd88af4479283fd
Jeongseok Lee <jslee02@gmail.com>
2018-07-25 09:32:22 -0700

    Fix boost_numpy_eigen name

--- Formula/boost_numpy_eigen.rb
+++ Formula/boost_numpy_eigen.rb
@@ -1,11 +1,12 @@
-class BoostEigenNumpy < Formula
-  desc "Boost.Python bindings that convert between NumPy and Eigen"
+class BoostNumpyEigen < Formula
+  desc "Boost.Python bindings for converting between NumPy and Eigen"
   homepage "https://github.com/personalrobotics/boost_numpy_eigen/"
   url "https://github.com/personalrobotics/boost_numpy_eigen/archive/v0.0.5.tar.gz"
-  sha256 "121420dfdf1ba884903ac3733c7c02639955c901c2074e6a5c677c67944d5475"
+  sha256 "affe9eb0647e63b2ab8e208e03f78d62bd9d8e0d34620761d464eb03dbe45c06"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "boost"
   depends_on "boost-python3"
   depends_on "eigen"
   depends_on "numpy"
@@ -19,6 +20,6 @@ class BoostEigenNumpy < Formula
   end
 
   test do
-    system python, "-c", "import boost_eigen_numpy"
+    system python, "-c", "import boost_numpy_eigen"
   end
 end
